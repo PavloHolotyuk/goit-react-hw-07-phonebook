b69664397505abc77dd8785538be656d20c84cb0
PavloHolotyuk <pavloholotuk@gmail.com>
2023-04-07 12:27:20 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,22 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit"
-import { contactsReducer } from "./contacts/contactsSlice"
+import { configureStore } from '@reduxjs/toolkit';
+import { contactsReducer } from './contacts/contactsSlice';
 // , persistedContactsReducer
-import { filterReducer } from "./filter/filterSlice"
-import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist"
+import { filterReducer } from './filter/filterSlice';
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 // persistStore,
 
 export const store = configureStore({
-	reducer: {
-		// contacts: persistedContactsReducer,
-		contacts: contactsReducer,
-		filter: filterReducer,
-	},
-	middleware: (getDefaultMiddleware) =>
-		getDefaultMiddleware({
-			serializableCheck: {
-				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-			},
-		}),
-})
-
-// export const persistor = persistStore(store)
+  reducer: {
+    contacts: contactsReducer,
+    filter: filterReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
