4e47c5e234c56bc22f4497796e3ed719d4e8c1d3
PavloHolotyuk <pavloholotuk@gmail.com>
2023-04-07 12:27:12 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,15 @@
-import ContactForm from "./ContactForm/ContactForm"
-import ContactList from "./ContactList/ContactList"
-import Filter from "./Filter/Filter"
-import styles from "./App.module.css"
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+import styles from './App.module.css';
 
 export function App() {
-	return (
-		<div className={styles.container}>
-			<ContactForm />
-			<Filter />
-      {/* {contacts.length > 0 ? (
-          <Filter />
-        ) : (
-          <p className={styles.info}>There is no contacts in a Phone Book</p>
-        )} */}
-			<ContactList />
-		</div>
-	)
+  return (
+    <div className={styles.container}>
+      <ContactForm />
+      <Filter />
+      {}
+      <ContactList />
+    </div>
+  );
 }
